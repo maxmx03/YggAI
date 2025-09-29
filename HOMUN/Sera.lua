119637aa0fca8d41965b30a78f50c52367755dc9
Max Miliano <milianordelcanto@gmail.com>
2025-09-28 23:20:22 -0300

    refactor(sera): conditional,default behavior

--- HOMUN/Sera.lua
+++ HOMUN/Sera.lua
@@ -2,6 +2,7 @@
 local node = require('AI.USER_AI.BT.nodes')
 ---@type Condition
 local condition = require('AI.USER_AI.BT.conditions')
+local Homun = require('AI.USER_AI.UTIL.Homun')
 
 ---@class Cooldown
 local MyCooldown = {
@@ -67,86 +68,68 @@ local MySkills = {
   },
 }
 
----@param mySkill number
-local isSkillCastable = function(mySkill)
-  MySkill = mySkill
-  ---@type Skill
-  local skill = MySkills[MySkill]
-  local cooldown = MyCooldown[MySkill]
-  return condition.isSkillCastable(skill, cooldown)
-end
-
----@param skill number
----@param target number
----@param opts SkillOpts
----@return Status
-local cast = function(skill, target, opts)
-  local casted = node.castSkill(MySkills[skill], MyCooldown[skill], target, opts)
-  if casted == STATUS.RUNNING then
-    MyCooldown[skill] = GetTickInSeconds()
-    return STATUS.RUNNING
-  elseif casted == STATUS.SUCCESS then
-    MyCooldown[skill] = GetTickInSeconds()
-    MySkill = 0
-    return STATUS.SUCCESS
-  end
-  return STATUS.FAILURE
-end
+---@type Homun
+local sera = Homun(MySkills, MyCooldown)
 
 local paralyse = {}
-function paralyse.condition()
-  if math.random(1, 100) <= 45 then
-    return isSkillCastable(MH_NEEDLE_OF_PARALYZE)
+function paralyse.isSkillCastable()
+  if math.random(1, 100) <= 40 then
+    return sera.isSkillCastable(MH_NEEDLE_OF_PARALYZE)
   end
   return false
 end
-function paralyse.cast()
-  return cast(MH_NEEDLE_OF_PARALYZE, MyEnemy, { targetType = 'target', keepRunning = true })
+function paralyse.castSkill()
+  return sera.castSkill(MH_NEEDLE_OF_PARALYZE, MyEnemy, { targetType = 'target', keepRunning = true })
 end
 local poison = {}
-function poison.condition()
-  return isSkillCastable(MH_POISON_MIST)
+function poison.isSkillCastable()
+  return sera.isSkillCastable(MH_POISON_MIST)
 end
-function poison.cast()
-  return cast(MH_POISON_MIST, MyEnemy, { targetType = 'ground', keepRunning = false })
+function poison.castSkill()
+  return sera.castSkill(MH_POISON_MIST, MyEnemy, { targetType = 'ground', keepRunning = false })
 end
 
 local pain = {}
-function pain.condition()
-  return isSkillCastable(MH_PAIN_KILLER)
+function pain.isSkillCastable()
+  return sera.isSkillCastable(MH_PAIN_KILLER)
 end
-function pain.cast()
-  return cast(MH_PAIN_KILLER, MyID, { targetType = 'target', keepRunning = false })
+function pain.castSkill()
+  return sera.castSkill(MH_PAIN_KILLER, MyID, { targetType = 'target', keepRunning = false })
 end
 local legion = {}
-function legion.condition()
-  return isSkillCastable(MH_SUMMON_LEGION)
+function legion.isSkillCastable()
+  return sera.isSkillCastable(MH_SUMMON_LEGION)
 end
-function legion.cast()
-  return cast(MH_SUMMON_LEGION, MyEnemy, { targetType = 'target', keepRunning = false })
+function legion.castSkill()
+  return sera.castSkill(MH_SUMMON_LEGION, MyEnemy, { targetType = 'target', keepRunning = false })
 end
-local AttackAndChaseParalyze = Parallel({
-  Conditions(node.basicAttack, Inversion(paralyse.condition)),
-  node.chaseEnemy,
-})
-local tryParaliseEnemy = Parallel({
-  Conditions(paralyse.cast, paralyse.condition),
-  node.chaseEnemy,
-})
-local invokeLegion = Parallel({
-  Conditions(legion.cast, legion.condition),
-  node.chaseEnemy,
-})
+
+local castPoisonMist = Condition(
+  Parallel({
+    poison.castSkill,
+    node.chaseEnemy,
+  }),
+  poison.isSkillCastable
+)
+local tryParaliseEnemy = Condition(
+  Parallel({
+    paralyse.castSkill,
+    node.chaseEnemy,
+  }),
+  paralyse.isSkillCastable
+)
+local invokeLegion = Condition(
+  Parallel({
+    legion.castSkill,
+    node.chaseEnemy,
+  }),
+  legion.isSkillCastable
+)
 local combat = Selector({
-  Conditions(invokeLegion, condition.isMVP),
-  Conditions(tryParaliseEnemy, paralyse.condition, Inversion(poison.condition)),
-  Conditions(poison.cast, poison.condition, condition.enemyIsAlive),
-  Conditions(pain.cast, pain.condition, condition.enemyIsAlive),
-  Conditions(AttackAndChaseParalyze, Inversion(paralyse.condition), condition.enemyIsAlive),
+  Condition(invokeLegion, condition.isMVP),
+  castPoisonMist,
+  Condition(tryParaliseEnemy, Inversion(poison.isSkillCastable)),
+  Condition(pain.castSkill, pain.isSkillCastable),
+  Condition(node.attackAndChase, Inversion(paralyse.isSkillCastable)),
 })
-local sera = Selector({
-  Conditions(combat, condition.hasEnemyOrInList, condition.ownerIsNotTooFar),
-  Conditions(node.follow, condition.ownerMoving),
-  Conditions(node.patrol, condition.ownerIsSitting, Inversion(condition.hasEnemyOrInList)),
-})
-return Condition(sera, IsSera)
+return Condition(sera.root(combat), IsSera)
--- config.lua
+++ config.lua
@@ -1,3 +1,3 @@
-MyLevel = 140
+MyLevel = 140 -- level do homunculus
 LifCanHeal = false -- LIF pode usar curar? true ou false (requer poção compacta)
 ShouldPreventHomunculusDuplication = false -- ou false
